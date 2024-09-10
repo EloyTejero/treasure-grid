cd64028ae91a6604569e38f1388e2690158d8447
EloyTejero <tejero.eloyet36@gmail.com>
2024-09-10 12:21:35 -0300

    GUI botonera, optimizacion un solo listener

--- src/view/Ventana.java
+++ src/view/Ventana.java
@@ -1,28 +1,44 @@
 package view;
 
-import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Ventana extends JFrame{
     
     public Ventana(){
-        setSize(500,500);
-        setLayout(null);
-        setVisible(true);
-        setResizable(false);
+        setSize(500,400);
+        setMinimumSize(new Dimension(500,400));
+        //setLayout(null);
+        //setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JPanel panel = new JPanel();
-        panel.setBounds(0, 0, 500, 300);
-        panel.setBackground(Color.red);
+        panel.setLayout(new GridLayout(10, 10)); // Cuadrícula de 4x4
+        
+        ActionListener clickListener = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JButton source = (JButton) e.getSource();
+                    System.out.println("Clic en: " + source.getText());
+                }
+        };
+        
+        // Añadir botones a la cuadrícula
+        for (int i = 1; i <= 100; i++) {
+            JButton button = new JButton(""+i);
+            button.addActionListener(clickListener);
+            panel.add(button);
+        }
+
+        // Añadir el panel al marco
         add(panel);
         
-        JLabel label = new JLabel("a");
-        label.setBounds(50, 0, 50, 50);
-        label.setBackground(Color.blue);
-        panel.add(label);
+        setVisible(true);
     }
     
     public static void main(String[] args){
